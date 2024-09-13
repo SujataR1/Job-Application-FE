a9f266bb243fca0105e6ce01bd8155fb813f2031
Transmogrify . <trsns@transmogrify06@outlook.com>
2024-09-13 13:56:09 +0530

    home page

--- job-application/src/App.js
+++ job-application/src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import HomePage from './components/Home/Homepage';
 
-// Import your components
-import HomePage from './components/Homepage';
-import JobListing from './components/JobListing';
-import FeaturedJobs from './components/FeaturedJobs';
-import LoginPage from './components/Authentication/Login/Login';
-import Navbar from './components/Universal/Navbar/Navbar'; // Import the Navbar component
-
-// App Component
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        {/* Render Navbar for all routes */}
-        <Navbar />
-
-        {/* Define your routes */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/jobs" element={<JobListing />} />
-          <Route path="/featured-jobs" element={<FeaturedJobs />} />
-          <Route path="/login" element={<LoginPage />} />
-          {/* Add additional routes as necessary */}
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                {/* Add more routes here as needed */}
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
